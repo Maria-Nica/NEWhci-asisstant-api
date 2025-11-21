b3fa2aa8c8eaa3161a7b265a6144a230e2cf833d
Maria-Nica <maria.nica.cristina@gmail.com>
2025-11-21 13:38:00 +0200

    mesaj

--- src/HCI.AIAssistant.API/Controllers/AIAssistantController.cs
+++ src/HCI.AIAssistant.API/Controllers/AIAssistantController.cs
@@ -1,10 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using HCI.AIAssistant.API.Models.DTOs.AIAssistantController;
-using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
+
+using HCI.AIAssistant.API.Models.DTOs.AIAssistantController;
+using HCI.AIAssistant.API.Services;
+using HCI.AIAssistant.API.Models.DTOs;
 
 namespace HCI.AIAssistant.API.Controllers;
 
@@ -12,13 +10,44 @@ namespace HCI.AIAssistant.API.Controllers;
 [Route("api/[controller]")]
 public class AIAssistantController : ControllerBase
 {
-    [HttpPost("/message")]
-    public async Task<ActionResult<AIAsisstantControllerPostMessageResponseDTO>> PostMessageAsync([FromBody] AIAsisstantControllerPostMessageRequestDTO request)
+    private readonly IAIAssistantService _aIAssistantService;
+    private readonly IParametricFunctions _parametricFunctions;
+
+    public AIAssistantController(
+        IAIAssistantService aIAssistantService,
+        IParametricFunctions parametricFunctions
+    )
     {
+        _aIAssistantService = aIAssistantService;
+        _parametricFunctions = parametricFunctions;
+    }
+
+    [HttpPost("/message")]
+    [ProducesResponseType(typeof(AIAsisstantControllerPostMessageResponseDTO), 200)]
+    [ProducesResponseType(typeof(ErrorResponseDTO), 400)]
+    public async Task<ActionResult> PostMessage([FromBody] AIAsisstantControllerPostMessageRequestDTO request)
+    {
+        if (!_parametricFunctions.ObjectExistsAndHasNoNullPublicProperties(request))
+        {
+            return BadRequest(
+                new ErrorResponseDTO()
+                {
+                    TextErrorTitle = "AtLeastOneNullParameter",
+                    TextErrorMessage = "Some parameters are null/missing.",
+                    TextErrorTrace = _parametricFunctions.GetCallerTrace()
+                }
+            );
+        }
+
+#pragma warning disable CS8604
+        string textMessageResponse = await _aIAssistantService.SendMessageAndGetResponseAsync(request.TextMessage);
+#pragma warning restore CS8604
+
         AIAsisstantControllerPostMessageResponseDTO response = new()
         {
-            TextMessage = "Hi" + request.TextMessage
+            TextMessage = textMessageResponse
         };
+
         return Ok(response);
     }
 }
