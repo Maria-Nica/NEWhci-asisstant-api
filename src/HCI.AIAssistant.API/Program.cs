f46a48d738fb1259c8916c5a34d4e4340f158610
Maria-Nica <maria.nica.cristina@gmail.com>
2025-11-28 15:21:15 +0200

    mesaj

--- src/HCI.AIAssistant.API/Program.cs
+++ src/HCI.AIAssistant.API/Program.cs
@@ -5,6 +5,19 @@ using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: "CORS",
+    policy =>
+    {
+        policy
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowAnyOrigin();
+    });
+});
+
+
 // Replace appsettings.json values with Key Vault values 
 var keyVaultName = builder.Configuration[$"AppConfigurations{ConfigurationPath.KeyDelimiter}KeyVaultName"]; var secretsPrefix = builder.Configuration[$"AppConfigurations{ConfigurationPath.KeyDelimiter}SecretsPrefix"]; if (string.IsNullOrWhiteSpace(keyVaultName)) { throw new ArgumentNullException("KeyVaultName", "KeyVaultName is missing."); }
 if (string.IsNullOrWhiteSpace(secretsPrefix)) { throw new ArgumentNullException("SecretsPrefix", "SecretsPrefix is missing."); }
@@ -30,7 +43,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.UseCors("CORS");
 // Configure the HTTP request pipeline.
 // if (app.Environment.IsDevelopment())
 // {
